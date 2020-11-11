02699cf5ccc440d7c65e558bf48abb93c6f32a54
Takanori Watanabe <takanori5064@gmail.com>
2020-11-12 06:40:45 +0900

    add input and messages

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    match io::stdin().read_line(&mut guess) {
+        Ok(_s) => println!("You guessed: {}", guess),
+        Err(_error) => println!("Failed to read line"),
+    }
 }
